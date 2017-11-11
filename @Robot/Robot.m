9b1229b60a94770c5ababe5c08f7e5140cc779d1
Francesco <francesco.argentieri89@gmail.com>
2017-11-11 19:37:34 +0100

    add EKF

--- @Robot/Robot.m
+++ @Robot/Robot.m
@@ -1,7 +1,19 @@
 classdef Robot < handle
     %ROBOTMODEL Summary of this class goes here
     %   Detailed explanation goes here
-    
+
+    properties (Constant)
+        % Vehicle property
+        whellradius = 0.15; % dimension of whell [m]
+        interaxle = 0.56;   % dimension of interaxle[m] |---|
+
+        % Encoder quantization
+        enc_quantization = 2 * pi / 2600;
+        % encoder noise
+        enc_mu = 0;                           % mean
+        enc_sigma = 2 * (2 * pi / 2600) / 3;  % variance
+    end
+
     properties
         ID;
         % Dimension plot
@@ -20,42 +32,54 @@ classdef Robot < handle
         % Quantization effect
         quatizeffect_LeftEnc;
         quatizeffect_RightEnc;
-        
+
         % EKF
-        
-        
-        
-    end
-    
-    properties (Constant)
-        % Vehicle property
-        whellradius = 0.15; % dimension of whell [m]
-        interaxle = 0.56;   % dimension of interaxe[m] |---|
-        
-        % Encoder quantization
-        enc_quantization = 2 * pi / 2600;
-        % encoder noise
-        enc_mu = 0;                   % mean
-        enc_sigma = 2 * (2 * pi / 2600) / 3;  % variance
-    end
-    
+        EKF_q_est;
+        EKF_p;
+        EKF_Q;
+        EKF_NumS;
+        EKF_q_store;
+        q_est_p;
+        P_p
+
+      end
+
     methods
         % class constructor
-        function this = Robot(inputID)
+        function this = Robot(inputID, time, sampletime)
+          % initialize identification number of robot
             this.ID = inputID;
             fprintf('Initialize robot n: %i\n', this.ID);
+            
+           % initialize simulation time and sample
+           Dt = sampletime;     % Sampling time
+           this.t = 0:Dt:time; % Length of simulation
+
+          % initialize Extend Kalman Filter aka EKF
+          this.EKF_q_est = zeros(3,1);
+          this.EKF_p = 1e2 * eye(3);
+          this.EKF_Q = diag([this.enc_sigma^2, this.enc_sigma^2]);
+          this.EKF_NumS = length(this.t);
+          this.EKF_q_store = zeros(3, this.EKF_NumS);
+          this.EKF_q_store(:,1) = this.EKF_q_est;
         end
-        
+
+        % function to compute the kinematics
         % Kinematic simulation
         this = UnicycleKinematicMatlab(this, MdlInit, Vehicle)
         dy = UnicycleModel(this, t,y)
         makerobot(this)
-        
+
         % Encoder Simulation
         this = EncoderSim(this, Vehicle)
         this = EncoderNoise(this)
+
+        % function to compute Extend Kalman Filter
+        this = prediciton(this,i)
+        this = update(this,i)
+        this = store(this,i)
     end
-    
+
     methods (Static)
         % Kinematic simulation
         [v, omega] = UnicycleInputs(t)
---
+++ @Robot/prediction.m
@@ -0,0 +1,30 @@
+%% Prediction
+function this = prediction(this, i)
+%PREDICTION function to estimate the new position and orientation of robot in EKF algortihm
+% INPUT:
+%  this = refer to this object
+%  i = iterator from cycle
+% OUTPUT: none
+
+% set local variable form class
+Sensor.Enc.NoisyRight = this.quatizeffect_RightEnc;
+Sensor.Enc.NoisyLeft = this.quatizeffect_LeftEnc;
+
+% Angle increments
+DeltaEnc = [Sensor.Enc.NoisyRight(i) - Sensor.Enc.NoisyRight(i-1);
+            Sensor.Enc.NoisyLeft(i) - Sensor.Enc.NoisyLeft(i-1)];
+
+A = [1 0 -sin(this.EKF_q_est(3))*this.whellradius/2*(DeltaEnc(1) + DeltaEnc(2));
+     0 1 cos(this.EKF_q_est(3))*this.whellradius/2*(DeltaEnc(1) + DeltaEnc(2));
+     0 0 1];
+B = [cos(this.EKF_q_est(3))*this.whellradius/2, cos(this.EKF_q_est(3))*this.whellradius/2;
+     sin(this.EKF_q_est(3))*this.whellradius/2, sin(this.EKF_q_est(3))*this.whellradius/2;
+     this.whellradius/this.interaxle, -this.whellradius/this.interaxle];
+
+this.q_est_p = this.EKF_q_est + B * DeltaEnc;
+this.P_p = A * this.EKF_p * A' + B * this.EKF_Q * B';
+
+% delete local variable
+clear Sensor
+
+end
