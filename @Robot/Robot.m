11903125ddb7753bb8abc82c905e8d36d0794134
Francesco <francesco.argentieri89@gmail.com>
2017-11-12 12:17:54 +0100

    getter method

--- @Robot/Robot.m
+++ @Robot/Robot.m
@@ -2,9 +2,9 @@ classdef Robot < handle
     %ROBOTMODEL Summary of this class goes here
     %   Detailed explanation goes here
     
-    properties (Constant)
+    properties (Constant)%, Access = private)
         % Vehicle property
-        whellradius = 0.15; % dimension of whell [m]
+        wheelradius = 0.15; % dimension of whell [m]
         interaxle = 0.56;   % dimension of interaxle[m] |---|
         
         % Encoder quantization
@@ -12,10 +12,15 @@ classdef Robot < handle
         % encoder noise
         enc_mu = 0;                           % mean
         enc_sigma = 2 * (2 * pi / 2600) / 3;  % variance
-    end
-    
-    properties
         
+        
+        % drawing
+        length  = 1;
+        width   = 1;
+    end
+
+    properties% (Access = private)
+
         ID;
 
         t = [];
@@ -27,7 +32,7 @@ classdef Robot < handle
         % Quantization effect
         quatizeffect_LeftEnc;
         quatizeffect_RightEnc;
-        
+
         % EKF
         EKF_q_est;
         EKF_p;
@@ -36,20 +41,23 @@ classdef Robot < handle
         EKF_q_store;
         q_est_p;
         P_p
-        
+
     end
-    
+
     methods
         % class constructor
-        function this = Robot(inputID, time, sampletime)
+        function this = Robot(inputID, time, sampletime, initialposition)
             % initialize identification number of robot
             this.ID = inputID;
             fprintf('Initialize robot n: %3i\n', this.ID);
-            
+
             % initialize simulation time and sample
             Dt = sampletime;     % Sampling time
             this.t = 0:Dt:time;  % Length of simulation
             
+            % set initial position
+            this.q = initialposition;
+
             % initialize Extend Kalman Filter aka EKF
             this.EKF_q_est = zeros(3,1);
             this.EKF_p = 1e2 * eye(3);
@@ -58,23 +66,29 @@ classdef Robot < handle
             this.EKF_q_store = zeros(3, this.EKF_NumS);
             this.EKF_q_store(:,1) = this.EKF_q_est;
         end
-        
+
         % function to compute the kinematics
         % Kinematic simulation
         this = UnicycleKinematicMatlab(this, MdlInit, Vehicle)
         dy = UnicycleModel(this, t,y)
-        makerobot(this)
         
         % Encoder Simulation
         this = EncoderSim(this, Vehicle)
         this = EncoderNoise(this)
-        
+
         % function to compute Extend Kalman Filter
         this = prediciton(this,i)
         this = update(this,i)
         this = store(this,i)
+        
+        % plot function
+        makerobot(this,vehicle)
+        
+        % getter method to access proprerty class 
+        numsteps = getEKFstep(this)
+        
     end
-    
+
     methods (Static)
         % Kinematic simulation
         [v, omega] = UnicycleInputs(t)
