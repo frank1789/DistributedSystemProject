e5ac38473ffa766f1d99a50ea4411e94f979ff7d
Francesco <francesco.argentieri89@gmail.com>
2017-11-12 11:16:53 +0100

    :bug: not display trajectory

--- @Robot/makerobot.m
+++ @Robot/makerobot.m
@@ -1,12 +1,72 @@
-function makerobot(this)
-hold on
-rectangle('Position',[0 0 2 4],'Curvature',0.2)
-% rectangle('Position',[6 0 2 4],'Curvature',1)
-text(1.1,2.1,sprintf('ID: %i',this.ID))
+function makerobot(this, vehicle)
 
-quiver(1, 2, 0, 1, 'r')
-quiver(1,2,1,0,'g')
-quiver3(1,2,0,0,0,1,'b')
+% instaziate vehicle position
+x = vehicle(1);
+y = vehicle(2);
+orientation = vehicle(3);
+
+pos_x = x - this.length/2;
+pos_y = y - this.width/2;
+
+hold on
 axis equal
-hold off
+
+% set base vehicle display a circle radius equal to 1
+% robot represent the scheme of plot
+robot = rectangle('Position',[pos_x pos_y this.width this.length], 'Curvature', [1 1], 'FaceColor',[0.5 0.5 0.5 0.5]);
+
+% label print on robot the ID number
+label = text(pos_x + .1, pos_y + .1, sprintf('ID: %i',this.ID));
+
+% reference system
+
+% quiver(x + this.q(1)*cos(this.q(3)), y, 0, 1, 'r')
+
+% % g = hgtransform;
+% quiver(x,y,0,1,'Parent',g)
+% set(g,'Matrix',makehgtform('xrotate',this.q(3)))
+
+
+% quiver(x, y, 1, 0, 'g')
+% quiver3(x, y, 0, 0, 0, 1, 'b')
+
+% trajectory = plot(this.q(1,1), this.q(1,2), '--r');
+
+
+
+
+
+% draw the animation from starting to end
+trajectory = []; % set a new trajectory array
+
+for i=2:length(this.EKF_q_store)
+    
+    
+    % clean the previous reperesentation before start new frame
+    delete(robot);
+    delete(label);
+    
+    % Actual trajectory
+    if not(isempty(trajectory))
+        delete(trajectory);
+    end
+    
+    % Vehicle trajectory
+    trajectory = plot(this.EKF_q_store(1,i), this.EKF_q_store(2,i));
+    
+    % update the Vehicle and label plot
+    newX = this.EKF_q_store(1,i) - this.width; % compute new position in x
+    newY = this.EKF_q_store(2,i) - this.width; % compute new position in y
+    robot = rectangle('Position', [newX, newY - this.width, this.width, this.length], 'Curvature', [1 1], 'FaceColor',[0.5 0.5 0.5 0.5]);
+    label = text(newX + .1, newY + .1, sprintf('ID: %i',this.ID));
+    
+    % Update figures and process callbacks
+    drawnow;
 end
+
+% set the axis and off block layer
+hold off
+axis equal
+end
+
+
