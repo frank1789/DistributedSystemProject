8eed45d72474cd189b595a7edeb5111742fea4c5
Francesco <francesco.argentieri89@gmail.com>
2017-11-16 12:18:19 +0100

    update method

--- @Robot/tempame.m
+++ @Robot/tempame.m
@@ -1,7 +1,7 @@
-function this = tempame(this, ppoints, plines)
+function this = tempame(this, ppoints, plines, it)
 
 % initialize initial postion
-initposition = [this.q(1), this.q(2) this.q(3)]; %[posotion x, y, theta]
+initposition = [this.q(it,1), this.q(it,2) this.q(it,3)]; %[posotion x, y, theta]
 % initialize sector of angle laser theta
 laserTheta = pi/180*( -90 : this.laserAngularResolution : 90 );
 
@@ -15,9 +15,9 @@ laser = [ initposition, pi, this.laserAngularResolution*pi/180,...
 laserReadings = Sens_model_noise( ppoints, plines, laser(1,:));
 
 % remove the data that are too far away
-rhosOver20m = laserReadings(2,:) > 20;
+rhosOver4m = laserReadings(2,:) > this.lasermaxdistance;
 
-laserScan_xy =[ laserReadings(2,:).*cos(laserTheta);...
+this.laserScan_xy =[ laserReadings(2,:).*cos(laserTheta);...
     laserReadings(2,:).*sin(laserTheta) ];
-disp(laserScan_xy);
+disp(this.laserScan_xy);
 end
