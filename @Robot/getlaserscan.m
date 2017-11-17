b2f9bdddf53e541c8f4c7f9cc7fe5b62937fdada
Francesco <francesco.argentieri89@gmail.com>
2017-11-17 10:04:09 +0100

    :sparkles: fix return type

--- @Robot/getlaserscan.m
+++ @Robot/getlaserscan.m
@@ -1,8 +1,8 @@
-function [laserScan_xy] = getlaserscan(this)
+function [laserScan_xy] = getlaserscan(this, it)
 %GETLASERSCAN methot return the distance from laser scanner sensor
 % INPUT:
 %  this = refer to this object
 % OUTPUT:
 %     laserScan_xy (double, double) = [x, y] points' postion
-laserScan_xy = this.laserScan_xy;
+laserScan_xy = this.laserScan_xy{it};
 end
