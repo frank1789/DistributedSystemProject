b20b5f4d0aba7d870420762bd4b4c668130ab5fb
Unknown <giacomomazzaglia@hotmail.it>
2017-11-27 18:16:54 +0100

    First Lidar-Occ_Grid Integration

--- Start.m
+++ Start.m
@@ -10,12 +10,12 @@ addpath ('Cost Function', 'Occupacy grid from Image')
 MapName = 'map_square.mat';
 mapStuct = load( MapName );
 
-figure(800)
-hold on
-plotMap(mapStuct.map);
-hold off
-axis equal
-grid on
+% figure(800)
+% hold on
+% plotMap(mapStuct.map);
+% hold off
+% axis equal
+% grid on
 p=1;
 % Sampling time
 MdlInit.Ts = 0.05;
@@ -32,6 +32,27 @@ Vehicle.q{3} = [-7; 3; 0];
 %
 
 a  = Robot(1, MdlInit.T, MdlInit.Ts, Vehicle.q{p});
+
+theta=-pi/2:0.36*pi/180:pi/2;
+ris = 0.015;
+r0=4/ris;
+
+y=r0*sin(theta);
+x=r0*cos(theta);
+
+x_g=floor(x);
+y_g=floor(y);
+
+max_x = max(x_g);
+max_y = max(y_g);
+
+n_mis = 201;%length(laserScan_xy);
+
+occ_mat = zeros(max_x,2*max_y,n_mis);
+lid_mat = zeros(max_x,2*max_y,n_mis); 
+
+laserScan_xy = cell.empty;
+j=1;
 for ii = 1:1:nit
     if mod(ii,2) == 0
         a.scanenvironment(mapStuct.map.points, mapStuct.map.lines, ii);
@@ -40,6 +61,16 @@ for ii = 1:1:nit
 
     a.UnicycleKinematicMatlab(ii);
     a.EncoderSim();
+    
+ laserScan_xy{ii} = a.laserScan_xy(ii);
+    
+ if(isempty(laserScan_xy{1,ii}{1,1}) || all((all(isnan(laserScan_xy{1,ii}{1,1})))==1))
+ 
+ else
+     out = laserScan_xy{1,ii}{1,1}(:,all(~isnan(laserScan_xy{1,ii}{1,1})));
+     [ occ_mat(:,:,j)] = Occ_Grid( occ_mat(:,:,j),lid_mat(:,:,j),out);
+     j=j+1;  
+     end
 
     
 end
@@ -69,10 +100,8 @@ end
 %     robota{nrobot}.tempame(mapStuct.map.points, mapStuct.map.lines);
 %     pause(0.1)
 % end
-laserScan_xy = cell.empty;
-for k = 1:length(a.laserScan_xy)
-    laserScan_xy{k} = a.laserScan_xy(k);
-end
+
+
 %
 %
 % figure, clf, hold on
@@ -90,77 +119,72 @@ rf_z  = cell.empty;
 limit= cell.empty;
 laserbeam = cell.empty;
 theta2 = linspace(-90 * pi/180, 90 * pi/180, round(180/0.36));
+% 
+% 
+% for n= 1:1:length(a.t)
+%     subplot(2,1,1);
+%     axis([-10, 10, -10, 10]);
+%     hold on
+%     axis equal
+%     grid on
+%     plotMap(mapStuct.map);
+%     for j=1:1
+%         if n == 1
+%             [body{j}, label{j}, rf_x{j}, rf_y{j}, rf_z{j}] =a.makerobot(n);
+%             %             limit{j} = a.plotlaserbeam(n);
+%         else
+%             delete([body{j}, label{j}, rf_x{j}, rf_y{j}, rf_z{j}]);
+%             [body{j}, label{j}, rf_x{j}, rf_y{j}, rf_z{j}] =a.animate(n);
+%             %             limit{j} = a.plotlaserbeam(n);
+%             %             while 1
+%             %                 for i = 1:length(theta2)
+%             %                     delete(laserbeam{i});
+%             %                     laserbeam{i} = a.animatelaser(n)
+%             %                     ray = line([0 R*cos(theta2(i))],[0 R*sin(theta2(i))],'color','r');
+%             %                     endray = plot(R*cos(theta2(i)),R*sin(theta2(i)),'r*');
+%             %                     b = toc(a); % check timer
+%             %     disp(b);
+%             %     if b > (1/10)
+%             %                     drawnow % update screen every 1/30 seconds
+%             %         a = tic; % reset timer after updating
+%             %             disp(a);
+%             %         drawnow;
+%         end
+%     end
+%     hold off
+%     
+%     
+%     drawnow;
+%     subplot(2,1,2);
+%     grid on
+%     hold on
+%     
+%     % transform cell arry to vector and store in local variable cluodpoint
+%     cloudpoint = cell2mat(laserScan_xy{n});
+%     % verify cloudpoint is nonvoid vector
+%     if ~isempty(cloudpoint)
+%         cl_point = plot(cloudpoint(1,:),cloudpoint(2,:),'.b'); % plot
+%     end
+%     %     delete(cl_point);
+%     hold off
+%     axis equal
+%     grid on
+%     
+%     %     % store gif
+%     %     frame = getframe(h);
+%     %       im = frame2im(frame);
+%     %       [imind,cm] = rgb2ind(im,256);
+%     %       % Write to the GIF File
+%     %       if n == 1
+%     %           imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
+%     %       else
+%     %           imwrite(imind,cm,filename,'gif','WriteMode','append');
+%     %       end
+% end % end animation
+% 
 
-
-for n= 1:1:length(a.t)
-    subplot(2,1,1);
-    axis([-10, 10, -10, 10]);
-    hold on
-    axis equal
-    grid on
-    plotMap(mapStuct.map);
-    for j=1:1
-        if n == 1
-            [body{j}, label{j}, rf_x{j}, rf_y{j}, rf_z{j}] =a.makerobot(n);
-            %             limit{j} = a.plotlaserbeam(n);
-        else
-            delete([body{j}, label{j}, rf_x{j}, rf_y{j}, rf_z{j}]);
-            [body{j}, label{j}, rf_x{j}, rf_y{j}, rf_z{j}] =a.animate(n);
-            %             limit{j} = a.plotlaserbeam(n);
-            %             while 1
-            %                 for i = 1:length(theta2)
-            %                     delete(laserbeam{i});
-            %                     laserbeam{i} = a.animatelaser(n)
-            %                     ray = line([0 R*cos(theta2(i))],[0 R*sin(theta2(i))],'color','r');
-            %                     endray = plot(R*cos(theta2(i)),R*sin(theta2(i)),'r*');
-            %                     b = toc(a); % check timer
-            %     disp(b);
-            %     if b > (1/10)
-            %                     drawnow % update screen every 1/30 seconds
-            %         a = tic; % reset timer after updating
-            %             disp(a);
-            %         drawnow;
-        end
-    end
-    hold off
-    
-    
-    drawnow;
-    subplot(2,1,2);
-    grid on
-    hold on
-    
-    % transform cell arry to vector and store in local variable cluodpoint
-    cloudpoint = cell2mat(laserScan_xy{n});
-    % verify cloudpoint is nonvoid vector
-    if ~isempty(cloudpoint)
-        cl_point = plot(cloudpoint(1,:),cloudpoint(2,:),'.b'); % plot
-    end
-    %     delete(cl_point);
-    hold off
-    axis equal
-    grid on
-    
-    %     % store gif
-    %     frame = getframe(h);
-    %       im = frame2im(frame);
-    %       [imind,cm] = rgb2ind(im,256);
-    %       % Write to the GIF File
-    %       if n == 1
-    %           imwrite(imind,cm,filename,'gif', 'Loopcount',inf);
-    %       else
-    %           imwrite(imind,cm,filename,'gif','WriteMode','append');
-    %       end
-end % end animation
-
-
-
-% close all;
-% %%
-% % compute Occupacygrid and stored in a cell array
-% occupacygrid = cell.empty;
-% for iterator = 1:201
-% occupacygrid{iterator} = a.getoccupacygrid(iterator);
-% end
-% close all
+  for i=1:1:j-1
+  figure
+  mesh(occ_mat(:,:,i));
+  end
 
