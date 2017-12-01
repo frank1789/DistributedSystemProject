6d30607331fc0b617f6ea7085678c05d43a9c59f
Francesco <francesco.argentieri89@gmail.com>
2017-12-01 15:07:14 +0100

    add validate input args

--- @Robot/Robot.m
+++ @Robot/Robot.m
@@ -1,7 +1,7 @@
 classdef Robot < handle
     %ROBOT Summary of this class goes here
     %   Detailed explanation goes here
-    
+
     % Vehicle property
     properties (Constant, Access = private)
         wheelradius = 0.15; % dimension of whell [m]
@@ -9,7 +9,7 @@ classdef Robot < handle
         length  = 1;    % data for drawing
         width   = 1;    % data for drawing
     end
-    
+
     % Physical quantities
     properties (SetAccess = private, Hidden = false)
         ID;     % Name of the robot
@@ -17,7 +17,7 @@ classdef Robot < handle
         q = []; % position [m]
         u = []; % velocity vector [rectilinear[m/s] angular[rad/s]]
     end
-    
+
     % Virtual incremental encoder
     properties (Constant, Access = private)
         enc_quantization = 2 * (pi / 2600);   % encoder quantization
@@ -25,7 +25,7 @@ classdef Robot < handle
         enc_mu = 0;                           % mean
         enc_sigma = 2 * (2 * pi / 2600) / 3;  % variance
     end
-    
+
     properties (SetAccess = private, Hidden = false)
         RightEnc = []; % right encoder values
         LeftEnc = [];  % left encoder values
@@ -33,7 +33,7 @@ classdef Robot < handle
         quatizeffect_LeftEnc = [];  % right encoder measure
         quatizeffect_RightEnc = []; % left encoder measure
     end
-    
+
     % EKF quantities
     properties (SetAccess = private, Hidden = false)
         EKF_q_est;      % position estimation
@@ -44,7 +44,7 @@ classdef Robot < handle
         q_est_p;
         P_p;
     end
-    
+
     % definition laser sensor
     properties (Constant, Access = private)
         laserAngularResolution = 0.36;  % [deg] laser sensor parameters
@@ -54,31 +54,37 @@ classdef Robot < handle
         laser_rho_sigma     = 0.1;              % mean
         laser_theta_sigma   = 0.1 * (pi / 180); % variance
     end
-    
+
     properties (SetAccess = private, Hidden = false)
         C_l_xy = {};
         laserScan_xy = cell.empty; % contains scans at a certain location
         distance = cell.empty;  % contains distance at a certain location
         mindistance = 0.85; % [m] min distance to start move
     end
-    
+
     methods
         % class constructor
         function this = Robot(inputID, time, sampletime, initialposition)
+            % check input constructor for passed arguments 
+            validateattributes(inputID,{'double'},{'nonnegative'})
+            validateattributes(time,{'double'},{'nonnegative'})
+            validateattributes(sampletime,{'double'},{'nonnegative'})
+            validateattributes(initialposition,{'double'},{'3d'},3)
+
             % initialize identification number of robot
             this.ID = inputID;
             fprintf('Initialize robot n: %3i\n', this.ID);
-            
+
             % initialize simulation time and sample
             Dt = sampletime;     % Sampling time
             dimension = length(0:Dt:time);  % Length of simulation
             this.distance{1,dimension} = [];
             this.laserScan_xy{1,dimension} = [];
-            
+
             % set initial position
             this.q = initialposition;
             this.t = 0;
-            
+
             % initialize Extend Kalman Filter aka EKF
             this.EKF_q_est = zeros(3,1);
             this.EKF_P = 1e2 * eye(3);
@@ -87,7 +93,7 @@ classdef Robot < handle
             this.EKF_q_store = zeros(3, this.EKF_NumS);
             this.EKF_q_store(:,1) = this.EKF_q_est;
         end % definition constructor
-        
+
         % function to compute the kinematics simulation
         this = UnicycleKinematicMatlab(this, MdlInit, Vehicle);
         %         dy = UnicycleModel(this, t, y, piterator)
@@ -95,19 +101,19 @@ classdef Robot < handle
         %         % Encoder Simulation
         %         this = EncoderSim(this, Vehicle);
         %         this = EncoderNoise(this);
-        
+
         % function to compute Extend Kalman Filter
         this = prediciton(this, i);
         this = update(this, i);
         this = store(this, i);
-        
+
         % plot function
         [body, label, rf_x, rf_y, rf_z] = makerobot(this, t);
         [body, label, rf_x, rf_y, rf_z] = animate(this, it);
-        
+
         % getter method to access proprerty class
         numsteps = getEKFstep(this)
-        
+
         % method to comupte laser scansion of the environment
         this = scanenvironment(this, ppoints, plines, it);
         [laserScan_xy] = getlaserscan(this, it);
@@ -120,19 +126,19 @@ classdef Robot < handle
         %         [occupacygrid] = setoccupacygrid(this, it)
         %         this = getmeasure(this, it)
     end
-    
+
     methods (Access = private)
         % function to compute the kinematics simulation
         dy = UnicycleModel(this, t, y, piterator)
-        
+
         % Encoder Simulation
         this = EncoderSim(this, Vehicle);
         this = EncoderNoise(this);
-        
+
         % method to comupte laser scansion of the environment
         this = getmeasure(this, it)
     end
-    
+
     methods (Static, Access = private)
         [v, omega] = UnicycleInputs(t, pdistance) % Kinematic simulation
         [R] = rotationMatrix(theta) % compute rotation matrix in plane 2D
