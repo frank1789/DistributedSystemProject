a4fdbe0954f88204f2ed1dc078533e83cece53db
Francesco <francesco.argentieri89@gmail.com>
2017-12-04 15:43:24 +0100

    refactor

--- @Robot/UnicycleInputs.m
+++ @Robot/UnicycleInputs.m
@@ -1,28 +1,14 @@
-function [v, omega] = UnicycleInputs(t, pdistance, ptheta)
+function [v, omega] = UnicycleInputs(t, ptheta)
 %UNICYCLEINPUTS the method used in the calculation of the robot kinematics
 % calculates the speed and rotation. Integrates a condition to avoid
-% obstacles. The minimum safety distance is 0.75 [cm], on the contrary if
-% the environmental scan does not detect obstacles or places at a distance
-% greater than the minimum distance it can move.
+% obstacles. 
 % INPUT:
 %  this (object) = refer to this object
-%  pdistance (double) = distance from the obstacle
+%  ptheta (double) = steering angle
 % OUTPUT:
 %  v (double) = rectlinear velocity [m/s]
 %  omega(double) = angular velocity [rad/s]
 
-% persistent angle
-% 
-% 
-% if ptheta ~= 0 
-%     angle = ptheta;
-% else 
-%     angle = 0;
-% end
-
-%     persistent theta_u
-%    theta_u = [4 -6]; % target[x y]
-% if (pdistance > 1.5 || isnan(pdistance))
 if ptheta == 0
     v = 2 * ones(length(t),1);    % [m/s]
     omega = zeros(length(t),1);% [rad/s]
