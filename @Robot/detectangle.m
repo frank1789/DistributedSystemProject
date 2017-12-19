1274ef9a9db50c24a71cebabe5e26f6fb71010b9
Francesco <francesco.argentieri89@gmail.com>
2017-12-19 18:50:33 +0100

    :sparkles: tested and removed unnecessary local variable

--- @Robot/detectangle.m
+++ @Robot/detectangle.m
@@ -19,39 +19,27 @@ function this = detectangle(this, piterator)
 %  this(object) = refer to this object
 %  steerangle (double) = angle
 
-% if ~isempty(this.distance{piterator})
-%     this.mindistance = min(this.distance{piterator});
-%     fprintf('check distance: %f\n', this.mindistance);
-% end
 if ~isempty(this.distance{piterator})   % check vector of measure is non void
     % local variable
-    %     matrixdistance = [this.distance{piterator}; this.laserTheta]; % [distance angle]
     indexmindistance = find(this.distance{piterator}(1,:) == min(this.distance{piterator}(1,:))); %index of minimum distance
     n = (ceil(length(this.distance{piterator})/2)); % compute center vector'measure
-    %     mindistance = min(this.distance{piterator}(1,:)); % store minimum distance
+    % not use yet
     index = find(isnan(this.test(1,:)));
-    %     lastindex = indexmindistance;
-    %     lastindex == true && this.q(end,3) == this.steerangle
-    
-    i_lower  = index(index < (n - 1));
-    i_higher = index(index > (n + 1));
-    if min(this.distance{piterator}(1,:)) < 0.8 || min(this.distance{piterator}(1,:)) > -0.8 && ~isnan(this.distance{piterator}(1,n))
+    i_lower  = index(index < (n - 1)); % min index contain angle steering
+    i_higher = index(index > (n + 1)); % max index contain angle steering
+    if min(this.distance{piterator}(1,:)) < 0.45 || min(this.distance{piterator}(1,:)) > -0.45 && ~isnan(this.distance{piterator}(1,n))
         switch find(min(this.distance{piterator}(1,:))) > n
             case 0
                 this.steerangle = pi/2; %round(this.laserTheta(1,min(i_higher)),7);
-               
             case 1
-                this.steerangle = pi/2;%round(this.laserTheta(1,max(i_lower)),7);
-              
+                this.steerangle = pi/2; %round(this.laserTheta(1,max(i_lower)),7);
             otherwise
                 this.steerangle = pi;
-                
-        end
+        end % end switch
     else
-        this.steerangle = 0;
-        %     end % check mindistance
-    end
-    
+        this.steerangle = 0; % reset angle steering
+    end % check mindistance
+else
+    return % exit from funtctio if vector is empty
 end % if measure vector is empty
-% this.plotangle(end+1) = this.steerangle;
 end % function
