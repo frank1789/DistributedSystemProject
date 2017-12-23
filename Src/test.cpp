5acbbb0612e393497b24c64f8894277cdbe2ce23
Francesco <francesco.argentieri89@gmail.com>
2017-12-23 17:01:27 +0100

    develop interface c++

---
+++ Src/test.cpp
@@ -0,0 +1,100 @@
+#include "mex.h"
+#include <iostream>
+#include <vector>
+#include "PFM.hpp"
+#include "robotinterface.hpp"
+#include <thread>
+// #include "robotinterface.hpp"
+
+
+
+void mexFunction( int nlhs, mxArray *plhs[],
+  int nrhs, const mxArray *prhs[])
+  {
+    /* Define local variable set the data from robot */
+    Robot<double> *b = new Robot<double>;
+    double *fromrobot = new double;
+    double *point = new double;
+    double *laserScan = new double;
+    double *laserRes = new double;
+
+    fromrobot = mxGetPr(prhs[0]);
+
+    /* initilize variable from robot
+     * - X coordinate position
+     * - X coordinate position
+     * - theta orientation
+    */
+
+    b->XcurrentPostion = fromrobot[0];
+    b->YcurrentPostion = fromrobot[1];
+    b->currentOrientation = fromrobot[2];
+
+    //std::cout<< "x "<<b->XcurrentPostion <<std::endl;
+    //std::cout<< "y "<<b->YcurrentPostion <<std::endl;
+    //std::cout<< "o "<<b->currentOrientation  <<std::endl;
+
+
+    #define B_OUT plhs[0]
+
+    /* retrive variable target point
+      - X coordinate position
+      - X coordinate position
+      - theta orientation
+    */
+
+    point = mxGetPr(prhs[1]);
+    Point<double> *target = new Point<double>;
+    target->XPostion = point[0];
+    target->YPostion = point[1];
+    target->Orientation = point[2];
+    //std::cout<< target->XPostion <<std::endl;
+    //std::cout<< target->YPostion <<std::endl;
+    //std::cout<< target->Orientation  <<std::endl;
+
+    /* instance Potetianl Field Method */
+    PFM::PathPlanner *pfm = new PFM::PathPlanner(b, target);
+
+    /* retrive laser measure */
+    std::vector<double> *measure = new std::vector<double>;
+    laserScan = mxGetPr(prhs[2]);
+    {
+      int M = mxGetM(prhs[2]);
+      int N = mxGetN(prhs[2]);
+      setvector(laserScan, measure, N, M);
+    }
+
+    /* retrive angular laser resoultion */
+    std::vector<double> *laser = new std::vector<double>;
+    laserRes = mxGetPr(prhs[3]);
+    {
+      int M = mxGetM(prhs[3]);
+      int N = mxGetN(prhs[3]);
+      setvector(laserRes, laser, N, M);
+    }
+    // {
+    //   int M = mxGetM(prhs[2]);
+    //   int N = mxGetN(prhs[2]);
+    //   std::cout<< "M: "<<M<<" N:"<<N<<std::endl;
+    //   for (int n = 0; n < N; n++) {
+    //     for(int m = 0; m < M; m++) {
+    //       std::cout<< laserTheta[m + M*n] << " idx: "<<n + M*n<<std::endl;
+    //     }
+    //   }
+    // }
+
+  //  std::cout << pfm->distance() << std::endl;
+
+    //std::cout << pfm->getTotalPotential()<<std::endl;
+    // delete b, pfm, laserScan, laserRes;
+    // delete fromrobot;
+    // delete point;
+    
+    /* garbage collector */
+    {
+      delete laser;
+      delete measure;
+    }
+    //output
+    B_OUT = mxCreateDoubleScalar(PI);
+  }
