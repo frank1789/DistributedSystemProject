feb8bc25aa6bb21ee2098654e16313f8caffa04f
Francesco <francesco.argentieri89@gmail.com>
2017-12-26 23:18:24 +0100

    method to set point target

--- @Robot/setpointtarget.m
+++ @Robot/setpointtarget.m
@@ -1,5 +1,5 @@
 function this = setpointtarget(this, ppoint)
-% validateattributes(ppoint,{'numeric'},{'nonnegative'});
+validateattributes(ppoint,{'numeric'},{'nonnegative'});
 if length(ppoint) == 3
     this.target = ppoint;
 else
