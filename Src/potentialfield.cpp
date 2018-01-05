86c68bc6ed38e591a7b5032260194d2ea4d9b769
Francesco <francesco.argentieri89@gmail.com>
2018-01-05 16:15:29 +0100

    :sparkles: work

--- Src/potentialfield.cpp
+++ Src/potentialfield.cpp
@@ -5,30 +5,50 @@
 #include "PFM.hpp"
 #include "robotinterface.hpp"
 
+/*! \mainpage My Personal Index Page
+ *
+ * \section intro_sec Introduction
+ *
+ * This is the introduction.
+ *
+ * \section install_sec Installation
+ *
+ * \subsection step1 Step 1: Opening the box
+ *
+ * etc...
+ */
+
+/**\fn mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+ * @brief Potentialfield
+ * @param[int] nlhs
+ * @param[mxArray *] plhs[]
+ * @param[int] nrhs
+ * @param[mxArray *] prhs[]
+ */
 void mexFunction( int nlhs, mxArray *plhs[],
                  int nrhs, const mxArray *prhs[])
 {
     /* Define local variable set the data from robot */
-    Robot<double> *b = new Robot<double>;
+    Robot<double> *robot = new Robot<double>;
     double *fromrobot = new double;
     double *point = new double;
     double *laserScan = new double;
     double *laserRes = new double;
     fromrobot = mxGetPr(prhs[0]);
-
+    
     /* initilize variable from robot
      * - X coordinate position
      * - X coordinate position
      * - theta orientation
      */
-    b->XcurrentPostion = fromrobot[0];
-    b->YcurrentPostion = fromrobot[1];
-    b->currentOrientation = fromrobot[2];
-
-    #define B_OUT plhs[0]
-    #define B_OUT1 plhs[1]
+    robot->XcurrentPostion = fromrobot[0];
+    robot->YcurrentPostion = fromrobot[1];
+    robot->currentOrientation = fromrobot[2];
     
-
+#define B_OUT plhs[0]
+#define B_OUT1 plhs[1]
+    
+    
     /* retrive variable target point
      - X coordinate position
      - X coordinate position
@@ -39,10 +59,10 @@ void mexFunction( int nlhs, mxArray *plhs[],
     target->XPostion = point[0];
     target->YPostion = point[1];
     target->Orientation = point[2];
-
+    
     /* instance Potetianl Field Method */
-    PFM::PathPlanner *pfm = new PFM::PathPlanner(b, target);
-
+    PFM::PathPlanner *pfm = new PFM::PathPlanner(robot, target);
+    
     /* retrive laser measure */
     std::vector<double> *Xmeasure = new std::vector<double>;
     std::vector<double> *Ymeasure = new std::vector<double>;
@@ -50,61 +70,36 @@ void mexFunction( int nlhs, mxArray *plhs[],
     {
         long M = mxGetM(prhs[2]);
         long N = mxGetN(prhs[2]);
-        //std::cout<<"dimension measure:" << N <<", "<<M <<"\n";
         if (N != 0)
         {
-          static long length = N;
-          setvector2x2(laserScan, Xmeasure, Ymeasure, N, M);
-        //   {
-        //     for (std::vector<double>::iterator it = Xmeasure->begin() ; it != Xmeasure->end(); ++it)
-        //       std::cout << ' ' << *it;
-        //     std::cout << '\n';
-        //   }
-        //   {
-        //     for (std::vector<double>::iterator it = Ymeasure->begin() ; it != Ymeasure->end(); ++it)
-        //       std::cout << ' ' << *it;
-        //     std::cout << '\n';
-        //   }
+            static long length = N;
+            setvector2x2(laserScan, Xmeasure, Ymeasure, N, M);
         }
-//        else
-//        {
-//          Ymeasure->resize(N);
-//          Xmeasure->resize(N);
-//          std::fill(Ymeasure->begin(), Ymeasure->end(), 1);
-//          std::fill(Xmeasure->begin(), Xmeasure->end(), 1);
-//        }
     }
-
+    
     /* retrive angular laser resoultion */
     std::vector<double> *laser = new std::vector<double>;
     laserRes = mxGetPr(prhs[3]);
     {
         long M = mxGetM(prhs[3]);
         long N = mxGetN(prhs[3]);
-        //std::cout<<"laser: " << N <<", "<<M <<"\n";
         setvector(laserRes, laser, N, M);
-        // {
-        //   std::cout<<"laserRes:\n";
-        //   for (std::vector<double>::iterator it = laser->begin() ; it != laser->end(); ++it)
-        //     std::cout << ' ' << *it;
-        //   std::cout << '\n';
-        // }
     }
-
+    
     /* retrive velocity */
     double *velocity = new double;
     velocity = mxGetPr(prhs[4]);
-
-      /* compute Potetianl Field Method */
-      pfm->setTotalPotential(Xmeasure, Ymeasure, laser);
-      /* return mex function output */
-      B_OUT = mxCreateDoubleScalar(pfm->getSteerangle(velocity));
-      B_OUT1 = mxCreateDoubleScalar(pfm->getSpeed(velocity));
-
+    
+    /* compute Potetianl Field Method */
+    pfm->setTotalPotential(Xmeasure, Ymeasure, laser);
+    /* return mex function output */
+    B_OUT = mxCreateDoubleScalar(pfm->getSteerangle(velocity));
+    B_OUT1 = mxCreateDoubleScalar(pfm->getSpeed(velocity));
+    
     /* garbage collector */
     {
         delete pfm;
-        delete b;
+        delete robot;
         delete target;
         delete Xmeasure;
         delete Ymeasure;
