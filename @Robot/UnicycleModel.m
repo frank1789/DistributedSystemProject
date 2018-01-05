22b820826807e07c614609e9601f3224e6e96e49
Francesco <francesco.argentieri89@gmail.com>
2018-01-05 12:39:45 +0100

    removed not needed part

--- @Robot/UnicycleModel.m
+++ @Robot/UnicycleModel.m
@@ -4,12 +4,9 @@ function dy = UnicycleModel(this, t, y, piterator)
 xu = y(1);
 yu = y(2);
 thetau = y(3);
-if ~isempty(this.distance{piterator})
-[this.steerangle] = passadati([xu yu thetau],this.target,this.distance{piterator},this.laserTheta, 4); 
-fprintf("it: %3i, %5.5f\n",piterator,this.steerangle)
-end
 
-[v, omega] = this.UnicycleInputs(t, this.steerangle);
+[v, omega] = this.UnicycleInputs(t, this.speed, this.steerangle);
+
 
 % System kinematic
 xu_d = cos(thetau) * v;
