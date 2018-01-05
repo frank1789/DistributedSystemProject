bfc035963b69fff23af8e57ffdb4d0965c5d0e23
Francesco <francesco.argentieri89@gmail.com>
2018-01-05 12:38:39 +0100

    pass argument speed from class

--- @Robot/UnicycleInputs.m
+++ @Robot/UnicycleInputs.m
@@ -1,4 +1,4 @@
-function [v, omega] = UnicycleInputs(t, ptheta)
+function [v, omega] = UnicycleInputs(t, pspeed, ptheta)
 %UNICYCLEINPUTS the method used in the calculation of the robot kinematics
 % calculates the speed and rotation. Integrates a condition to avoid
 % obstacles.
@@ -10,10 +10,10 @@ function [v, omega] = UnicycleInputs(t, ptheta)
 %  omega(double) = angular velocity [rad/s]
 
 if ptheta == 0
-    v = 2 * ones(length(t),1);    % [m/s]
+    v = pspeed * ones(length(t),1);    % [m/s]
     omega = zeros(length(t),1);% [rad/s]
 else
-    v =   0.3 * ones(length(t),1); % [m/s]
+    v =   0.5 * ones(length(t),1); % [m/s]
     omega = ptheta * ones(length(t),1);% [rad/s]
 end
 % fprintf('actual speed:\t%.5f m/s; omega = \t%.5f\n', v, omega);
