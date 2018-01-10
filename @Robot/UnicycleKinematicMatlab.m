3ab4a4b1dcf78e1f7c845a427ba82896308bd895
Francesco <francesco.argentieri89@gmail.com>
2018-01-10 14:32:51 +0100

    correction

--- @Robot/UnicycleKinematicMatlab.m
+++ @Robot/UnicycleKinematicMatlab.m
@@ -18,27 +18,14 @@ function this = UnicycleKinematicMatlab(this, piterator)
 %  t(double) = last row of time computed by ode45 [n, m]
 %  q(double) = last row of time computed by ode45 [n, m]
 
-persistent t1 q; % allocate static varible to perform ode45
+persistent t1 q;             % allocate static varible to perform ode45
 if isempty(t1) && isempty(q) % check if void, otherwise it takes last solution ode45
     t1 = this.t;
     q = this.q;
 end
 
-if ~isempty(this.laserScan_xy{piterator})
-if sqrt((this.target(1) - q(end,1))^2 + (this.target(2) - q(end,2))^2) > 0.05
-%     if this.steerangle >= q(end,3) / 1.05 &&  this.steerangle <= q(end,3) * 1.05 %|| isnan(this.laserScan_xy{piterator}(1,251))
-disp(piterator);    
-[this.steerangle] = potentialfield(q(end,:),this.target,this.laserScan_xy{piterator},this.laserTheta, 0.5);
-     this.steerangle = wrapToPi(this.steerangle);
-%     end
-else
-    this.speed = 0;
-    this.steerangle = 0.0;
-end
-end
-
 % Unicycle dynamic
-[t1, q] = ode45(@(t, y) this.UnicycleModel(t, y), [t1(end) t1(end)+0.05], q(end,:));
+[t1, q] = ode45(@(t, y, it) this.UnicycleModel(t, y, piterator), [t1(end) t1(end)+0.05], q(end,:));
 q(end,3) = wrapToPi(q(end,3));
 this.q(end+1,:) = q(end,:); % store last row of solution - postion
 this.t(end+1) = t1(end);    % store last row of solution - time
@@ -47,5 +34,5 @@ this.t(end+1) = t1(end);    % store last row of solution - time
 % Input sequence
 [v, omega] = this.UnicycleInputs(this.t, this.speed, this.steerangle);
 this.u = [v'; omega']; % store vector of velocity
-this.EncoderSim(); % perform encoder simulation
+this.EncoderSim();     % perform encoder simulation
 end % method
