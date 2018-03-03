f88f5d208ac9686f59fd94da43eecec15be33c46
Francesco <francesco.argentieri89@gmail.com>
2018-03-03 15:53:35 +0100

    fix

--- src/DungeonGenerator/test.cpp
+++ src/DungeonGenerator/test.cpp
@@ -21,7 +21,7 @@ void mexFunction( int nlhs, mxArray *plhs[],
     std::cout << test <<std::endl;
     Dungeon d(static_cast<int>(*width), static_cast<int>(*height));
     d.generate(static_cast<int>(*room));
-     d.print();
+//     d.print();
     // read data from dungeon generator
     d.setPointRoom();
 //    d.setPointCorridor();
@@ -29,21 +29,28 @@ void mexFunction( int nlhs, mxArray *plhs[],
     int dim_room_point = 2* static_cast<int>(d.getRoom().size());
     // allocate return array
     double* point = new double[dim_room_point];
-    double* line = new double[dim_room_point];
     // fill the array
     copyarray(d.getRoom(), point, dim_room_point);
+    // generate line
     std::vector<int>* point_to_point = new std::vector<int>;
     connectpoint(d.getRoom(), point_to_point);
+    double* line = new double[point_to_point->size()];
     std::copy(point_to_point->begin(), point_to_point->end(), line);
-//    // return outputs from MEX-function
+    // preallocated return array
     plhs[0] = mxCreateDoubleMatrix(N_ROW, dim_room_point, mxREAL);
-    plhs[1] = mxCreateDoubleMatrix(N_ROW, point_to_point->size(), mxREAL);
-    memcpy(mxGetPr(plhs[0]), point, dim_room_point * sizeof(double));
-    memcpy(mxGetPr(plhs[1]), line, point_to_point->size() * sizeof(double));
-//    if (nlhs > 1) {
-//        plhs[1] = mxCreateDoubleScalar(dim_room_point);
-//    }
-//
+    plhs[1] = mxCreateDoubleMatrix(N_ROW, point_to_point->size()/2, mxREAL);
+    // return outputs from MEX-function
+    if (nlhs == 2)
+    {
+        memcpy(mxGetPr(plhs[0]), point, dim_room_point * sizeof(double));
+        memcpy(mxGetPr(plhs[1]), line, point_to_point->size() * sizeof(double));
+    }
+    else
+    {
+        mexErrMsgIdAndTxt("MyProg:ConvertString",
+                          "Two return variables are required.");
+        // exit MEX file
+    }
     // dellocate heap space
     delete [] point;
     delete [] line;
