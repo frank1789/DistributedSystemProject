a246025ccd5024a488770c3d8075a47f23063f5e
Francesco <francesco.argentieri89@gmail.com>
2018-03-08 11:17:10 +0100

    refactor

--- src/DungeonGenerator/interface.h
+++ src/DungeonGenerator/interface.h
@@ -3,43 +3,7 @@
 #define INTERFACE_H
 
 void connectpoint(const std::vector<geometry::i_point> &dungeonpoint, std::vector<int>* vec);
-void copyarray(const std::vector<geometry::i_point> &result, double *vector_in);
-void copyarray(const std::vector<geometry::i_point> &result, std::vector<int>* vector_in);
 
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-int getcolunm(const std::vector<geometry::i_point> &dungeonfeature);
-
-void copyarray(const std::vector<geometry::i_point> &dungeonpoint, double *vectorin, int size);
-
-
-void bo(Dungeon &dungeon);
-
-std::vector<geometry::i_point> sortOrdinates(std::vector<geometry::i_point> &vect);
-
-std::vector<geometry::i_point> sortAbscissa(std::vector<geometry::i_point> &vect);
-
-std::vector<geometry::f_point> sortAbscissa(std::vector<geometry::f_point> &vect);
-
-std::vector<geometry::f_point> sortOrdinates(std::vector<geometry::f_point> &vect);
-
-void assembleHorizzontalLine(std::vector<geometry::i_point>* p_room, std::vector<geometry::i_point>* p_door);
-
-void assembleVerticalLine(std::vector<geometry::i_point>* p_room,std::vector<geometry::i_point>* p_door);
-
-//class Interface
-//{
-//public:
-//  Interface(Dungeon &dungeon);
-//  ~Interface();
-//private:
-//  std::vector<geometry::i_point>* p_room;
-//  std::vector<geometry::i_point>* p_door;
-
-//}
-
-
-
+void copyarray(const std::vector<geometry::i_point> &v_in, std::vector<int>* v_out);
 
 #endif // INTERFACE_H
--- src/DungeonGenerator/test.cpp
+++ src/DungeonGenerator/test.cpp
@@ -10,69 +10,68 @@
 void mexFunction( int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
     // gateway value
-    double* width   = mxGetPr(prhs[0]);
-    double* height  = mxGetPr(prhs[1]);
-    double* room    = mxGetPr(prhs[2]);
-    std::cout << "start creating the map " << *width << " x " << *height <<std::endl;
-    std::cout << "take a while..." << std::endl;
-    
-    //generate dungeon - cast from double to int
-    Dungeon d(static_cast<int>(*width), static_cast<int>(*height));
-    d.generate(static_cast<int>(*room));
-    d.print();
-    
-    // read data from dungeon generator
-    d.setPointRoom();
-    
-    // set dimension for output MATLAB array, then allocate return array
-    std::vector<int>* rooms = new std::vector<int>;
-    copyarray(d.getRoom(), rooms);
-    double* points = new double [rooms->size()];
-    std::copy(rooms->begin(), rooms->end(), points); // copy room's points
-    
-    // set dimension for output MATLAB array, then allocate return array
-    std::vector<int>* corridor = new std::vector<int>;
-    copyarray(d.getCorridor(), corridor);
-    double* c_points = new double [corridor->size()];
-    std::copy(corridor->begin(), corridor->end(), c_points); // copy room's points
-    
-    // connect line
-    std::vector<int>* point_to_point = new std::vector<int>;
-    connectpoint(d.getRoom(), point_to_point);
-    double* line = new double[point_to_point->size()];
-    std::copy(point_to_point->begin(), point_to_point->end(), line);
-    
-    // connect line
-    std::vector<int>* cpoint_to_cpoint = new std::vector<int>;
-    connectpoint(d.getCorridor(), cpoint_to_cpoint);
-    double* c_line = new double[cpoint_to_cpoint->size()];
-    std::copy(cpoint_to_cpoint->begin(), cpoint_to_cpoint->end(), c_line);
-    
-    
-    // preallocated return array
-    plhs[0] = mxCreateDoubleMatrix(N_ROW, rooms->size()/2, mxREAL);
-    plhs[1] = mxCreateDoubleMatrix(N_ROW, point_to_point->size()/2, mxREAL);
-    plhs[2] = mxCreateDoubleMatrix(N_ROW, corridor->size()/2, mxREAL);
-    plhs[3] = mxCreateDoubleMatrix(N_ROW, cpoint_to_cpoint->size()/2, mxREAL);
-    
-    // return outputs from MEX-function
-    if (nlhs == 4)
-    {
-        memcpy(mxGetPr(plhs[0]), points, rooms->size() * sizeof(double));
-        memcpy(mxGetPr(plhs[1]), line, point_to_point->size() * sizeof(double));
-        memcpy(mxGetPr(plhs[2]), c_points, corridor->size() * sizeof(double));
-        memcpy(mxGetPr(plhs[3]), c_line, cpoint_to_cpoint->size() * sizeof(double));
-    }
+    if (nrhs == 3)
+        {
+            double* width   = mxGetPr(prhs[0]);
+            double* height  = mxGetPr(prhs[1]);
+            double* room    = mxGetPr(prhs[2]);
+            std::cout << "start creating the map " << *width << " x " << *height <<std::endl;
+            std::cout << "take a while..." << std::endl;
+
+            //generate dungeon - cast from double to int
+            Dungeon d(static_cast<int>(*width), static_cast<int>(*height));
+            d.generate(static_cast<int>(*room));
+            d.print();
+
+            // read data from dungeon generator
+            d.setPointRoom();
+            // set dimension for output MATLAB array, then allocate return array
+            std::vector<int>* rooms = new std::vector<int>;
+            std::vector<int>* wall = new std::vector<int>;
+            std::vector<int>* corridor = new std::vector<int>;
+            std::vector<int>* structure = new std::vector<int>;
+            copyarray(d.getRoom(), rooms);
+            copyarray(d.getWall(), wall);
+            copyarray(d.getCorridor(), corridor);
+            unsigned long dim = rooms->size() + wall->size() + corridor->size();
+            structure->reserve(dim);
+            structure->insert(structure->end(), rooms->begin(), rooms->end());
+            structure->insert(structure->end(), corridor->begin(), corridor->end());
+            structure->insert(structure->end(), wall->begin(), wall->end());
+            double* points = new double [structure->size()];
+            std::copy(structure->begin(), structure->end(), points); // copy room's points
+            // set dimension for output MATLAB array, then allocate return array
+            // connect line
+            std::vector<int>* point_to_point = new std::vector<int>;
+            connectpoint(d.getRoom(), point_to_point);
+            connectpoint(d.getCorridor(), point_to_point);
+            unsigned long dim_line = point_to_point->size() ;
+            double* line = new double[dim_line];
+            std::copy(point_to_point->begin(), point_to_point->end(), line);
+
+            // preallocated return array
+            plhs[0] = mxCreateDoubleMatrix(N_ROW, structure->size()/2, mxREAL);
+            plhs[1] = mxCreateDoubleMatrix(N_ROW, dim_line/2, mxREAL);
+
+            // return outputs from MEX-function
+            if (nlhs == 2)
+                {
+                    memcpy(mxGetPr(plhs[0]), points, structure->size() * sizeof(double));
+                    memcpy(mxGetPr(plhs[1]), line, dim_line * sizeof(double));
+                }
+            else // exit MEX file
+                mexErrMsgIdAndTxt("MyProg:ConvertString", "Four return variables are required.");
+
+
+            // dellocate heap space
+            delete [] points;
+            delete [] line;
+            delete rooms;
+            delete structure;
+            delete corridor;
+            delete wall;
+            delete point_to_point;
+        }
     else
-    {
-        mexErrMsgIdAndTxt("MyProg:ConvertString", "Two return variables are required.");
-        // exit MEX file
-    }
-    // dellocate heap space
-    delete [] points;
-    delete [] line;
-    delete [] c_points;
-    delete rooms;
-    delete point_to_point;
-    delete corridor;
+        mexErrMsgIdAndTxt("MyProg:ConvertString", "Three input parameters are required:\n\t width,\n\t height,\n\t number of rooms");
 }
