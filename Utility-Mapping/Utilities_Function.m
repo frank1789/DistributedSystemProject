02056ac41cd5c014be21b6d603f9f882de8e74c7
Francesco <francesco.argentieri89@gmail.com>
2018-05-04 17:47:06 +0200

    :bug: fix
    improve speed and reduce ram

--- Utility-Mapping/Utilities_Function.m
+++ Utility-Mapping/Utilities_Function.m
@@ -1,23 +1,19 @@
-function [ P ] = Utilities_Function( robot,idx_robot,n,max_range)
+function [ P ] = Utilities_Function(robot, idx_robot, max_range)
+%UTILIES_FUNCTION  calculates the utility value for the given robot target
+% and location respect the target and location of the other ones.
+%The formula is taken from the article "Coordinated Multi-Robot
+%Exploration".
 
-%This function calculates the utility value for the given robot target and
-%location respect the target and location of the other ones
-%The formula is taken from the article "Coordinated Multi-Robot Exploration"
-    P=0;
-    
-    for i=1:1:n
-        if(idx_robot~=i)
+P=0;
+for i=1:length(robot)
+    if(idx_robot~=i)
         d = norm(robot{idx_robot}.target(1:2) - robot{i}.target(1:2));
-            
-            if(d<max_range)
-                Pd = 1 - d/max_range;
-            else
-                Pd = 0;
-            end
-    
-         P = P + Pd;
+        if(d<max_range)
+            Pd = 1 - d/max_range;
+        else
+            Pd = 0;
         end
-  
+        P = P + Pd;
     end
-        
+end
 end
