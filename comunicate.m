73328537c285f21bc6cdcd420e0b8e7b8d080a79
Giacomo <giacomomazzaglia@hotmail.it>
2018-05-15 18:00:24 +0200

    visibility improved

--- comunicate.m
+++ comunicate.m
@@ -17,7 +17,7 @@ switch length(Robot)
         
     case 2
         [dist] = euclideandistance(c{1,1}.q(it,:),c{1,2}.q(it,:));
-        if (dist < MAX && occparameters{c{1,1}.ID}.comunication ==0 && occparameters{c{1,2}.ID}.comunication ==0)
+        if (dist < MAX && (occparameters{c{1,1}.ID}.comunication ==0 || occparameters{c{1,2}.ID}.comunication ==0))
             fprintf("one or more robots are in the communication area\n");
             fprintf("establish link\t robot: %i <---> robot: %i\n",c{1,1}.ID,c{1,2}.ID);
             %exchange laserscan and odometry
@@ -25,14 +25,14 @@ switch length(Robot)
             occparameters{c{1,1}.ID}.comunication =1;
             occparameters{c{1,2}.ID}.comunication =1;
             
-            occparameters{c{1,1}.ID}.Cost_map = (occparameters{c{1,1}.ID}.Cost_map + occparameters{c{1,2}.ID}.Cost_map)/2;
-            occparameters{c{1,2}.ID}.Cost_map = (occparameters{c{1,1}.ID}.Cost_map + occparameters{c{1,2}.ID}.Cost_map)/2;
+            occparameters{c{1,1}.ID}.Cost_map(occparameters{c{1,2}.ID}.Cost_map) = 2;
+            occparameters{c{1,2}.ID}.Cost_map(occparameters{c{1,1}.ID}.Cost_map) = 2;
         end
         
     otherwise
         for n = 1:length(c)
             [dist] = euclideandistance(c{n,1}.q(it,:),c{n,2}.q(it,:));
-            if (dist < MAX && occparameters{c{n,1}.ID}.comunication ==0 && occparameters{c{n,2}.ID}.comunication ==0)
+            if (dist < MAX && (occparameters{c{n,1}.ID}.comunication ==0 || occparameters{c{n,2}.ID}.comunication ==0))
                 fprintf("one or more robots are in the communication area\n");
                 fprintf("establish link\t robot: %i <---> robot: %i\n",c{n,1}.ID,c{n,2}.ID);
                 %exchange laserscan and odometry
@@ -40,9 +40,8 @@ switch length(Robot)
                 occparameters{c{n,1}.ID}.comunication =1;
                 occparameters{c{n,2}.ID}.comunication =1;
                 
-                occparameters{c{n,1}.ID}.Cost_map = (occparameters{c{n,1}.ID}.Cost_map + occparameters{c{n,2}.ID}.Cost_map)/2;
-                occparameters{c{n,2}.ID}.Cost_map = (occparameters{c{n,1}.ID}.Cost_map + occparameters{c{n,2}.ID}.Cost_map)/2;
-            end % if
+            occparameters{c{1,1}.ID}.Cost_map(occparameters{c{1,2}.ID}.Cost_map) = 2;
+            occparameters{c{1,2}.ID}.Cost_map(occparameters{c{1,1}.ID}.Cost_map) = 2;  end % if
         end % for
 end
 clear c dist % free memory
